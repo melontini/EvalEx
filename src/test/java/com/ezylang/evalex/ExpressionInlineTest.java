b9a03b01e28da79fbe31aad25ca26f513a2667ef
melontini <104443436+melontini@users.noreply.github.com>
2024-07-22 02:06:50 +0700

    More inlining tests.

--- src/test/java/com/ezylang/evalex/ExpressionInlineTest.java
+++ src/test/java/com/ezylang/evalex/ExpressionInlineTest.java
@@ -26,34 +26,62 @@ import org.junit.jupiter.api.Test;
 public class ExpressionInlineTest extends BaseExpressionEvaluatorTest {
 
   @Test
-  public void testSimpleInlinedExpression() throws ParseException, EvaluationException {
+  public void testInlinedExpression() throws ParseException, EvaluationException {
     Expression expression = parser.inlineExpression(createExpression("2 + 2"));
 
-    assertThat(expression.evaluate(UnaryOperator.identity()).getNumberValue())
-        .isEqualTo(BigDecimal.valueOf(4));
+    assertThat(expression.evaluate(UnaryOperator.identity()).getStringValue())
+        .isEqualTo(BigDecimal.valueOf(4).toPlainString());
     assertThat(expression.getAbstractSyntaxTree()).isInstanceOf(InlinedASTNode.class);
   }
 
   @Test
-  public void testConstantInlinedExpression() throws ParseException, EvaluationException {
+  public void testInlinedConstant() throws ParseException, EvaluationException {
     Expression expression = parser.inlineExpression(createExpression("2 + PI"));
 
-    assertThat(expression.evaluate(UnaryOperator.identity()).getNumberValue())
+    assertThat(expression.evaluate(UnaryOperator.identity()).getStringValue())
         .isEqualTo(
             BigDecimal.valueOf(2)
                 .add(
                     new BigDecimal(
-                        "3.1415926535897932384626433832795028841971693993751058209749445923078")));
+                        "3.1415926535897932384626433832795028841971693993751058209749445923078"))
+                .toPlainString());
     assertThat(expression.getAbstractSyntaxTree()).isInstanceOf(InlinedASTNode.class);
   }
 
   @Test
-  public void testParameterNotInlinedExpression() throws ParseException, EvaluationException {
+  public void testNotInlinedParameter() throws ParseException, EvaluationException {
     Expression expression = parser.inlineExpression(createExpression("cheese / 2"));
 
-    assertThat(expression.evaluate(builder -> builder.parameter("cheese", 22)).getNumberValue())
+    assertThat(expression.evaluate(builder -> builder.parameter("cheese", 22)).getStringValue())
         .isEqualTo(
-            BigDecimal.valueOf(22).divide(BigDecimal.valueOf(2), configuration.getMathContext()));
+            BigDecimal.valueOf(22)
+                .divide(BigDecimal.valueOf(2), configuration.getMathContext())
+                .toPlainString());
+    assertThat(expression.getAbstractSyntaxTree()).isNotInstanceOf(InlinedASTNode.class);
+  }
+
+  @Test
+  public void testFunctionInlined() throws ParseException, EvaluationException {
+    Expression expression = parser.inlineExpression(createExpression("SUM(2, 4, 6, 7)"));
+
+    assertThat(expression.evaluate(UnaryOperator.identity()).getStringValue())
+        .isEqualTo(BigDecimal.valueOf(2 + 4 + 6 + 7).toPlainString());
+    assertThat(expression.getAbstractSyntaxTree()).isInstanceOf(InlinedASTNode.class);
+  }
+
+  @Test
+  public void testFunctionNotInlined() throws ParseException, EvaluationException {
+    Expression expression = parser.inlineExpression(createExpression("SUM(2, a, 6, 7)"));
+
+    assertThat(expression.evaluate(builder -> builder.parameter("a", 5)).getStringValue())
+        .isEqualTo(BigDecimal.valueOf(2 + 5 + 6 + 7).toPlainString());
+    assertThat(expression.getAbstractSyntaxTree()).isNotInstanceOf(InlinedASTNode.class);
+  }
+
+  @Test
+  public void testRandomNotInlined() throws ParseException, EvaluationException {
+    Expression expression = parser.inlineExpression(createExpression("RANDOM()"));
+
     assertThat(expression.getAbstractSyntaxTree()).isNotInstanceOf(InlinedASTNode.class);
   }
 }
